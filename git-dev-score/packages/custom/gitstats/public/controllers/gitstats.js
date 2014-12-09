5ed1d0de3b8796dd4b9484ed5c5bd076a5716a92
RaymondKlass <raymond@klassphoto.com>
2014-12-09 18:45:58 -0500

    hook up service to UI

--- git-dev-score/packages/custom/gitstats/public/controllers/gitstats.js
+++ git-dev-score/packages/custom/gitstats/public/controllers/gitstats.js
@@ -1,10 +1,19 @@
 'use strict';
 
-angular.module('mean.gitstats').controller('GitstatsController', ['$scope', 'Global', 'Gitstats',
+angular.module('mean.gitstats').controller('GitstatsController', ['$scope', 'Global', 'Gitstats', 
   function($scope, Global, Gitstats) {
     $scope.global = Global;
     $scope.package = {
       name: 'gitstats'
     };
+    $scope.message = 'No Message Yet...';
+    
+    
+    $scope.getMessage = function() {
+      Gitstats.get(function(data) {
+        $scope.message = data.msg;
+      });
+    };
+    
   }
 ]);
