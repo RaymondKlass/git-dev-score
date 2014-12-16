e8d75f3842d229cb07896c168b6e69186556d99b
RaymondKlass <raymond@klassphoto.com>
2014-12-15 21:23:52 -0500

    basic setup - testing Jasmine

--- git-dev-score/packages/custom/gitstats/public/tests/gitstats.spec.js
+++ git-dev-score/packages/custom/gitstats/public/tests/gitstats.spec.js
@@ -1,1 +1,36 @@
 'use strict';
+
+(function() {
+
+    // Gitstats Spec
+    describe('Angular Controllers', function() {
+        describe('Gitstats Controllers', function() {
+            // Add a custome matcher that ignores functions in comparisons (since AngularJS $resource add methods)
+            beforeEach(function() {
+                this.addMatchers({
+                    toEqualData: function(expected) {
+                        return angular.equals(this.actual, expected);
+                    }
+                });
+            });
+            
+            beforeEach(function() {
+                module('mean');
+                module('mean.system');
+                module('mean.gitstats');
+            });
+            
+            // Initialize controller and mock scope
+            var gitstatsController,
+                scope,
+                $httpBackend,
+                $stateParams,
+                $location;
+            
+            
+            
+        });
+    
+    });
+
+}());
