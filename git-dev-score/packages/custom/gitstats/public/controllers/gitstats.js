0f933bebf35e4596c8ad0fff9bff5e7b135d51e7
RaymondKlass <raymond@klassphoto.com>
2014-12-20 14:08:16 -0500

    add git dev username function

--- git-dev-score/packages/custom/gitstats/public/controllers/gitstats.js
+++ git-dev-score/packages/custom/gitstats/public/controllers/gitstats.js
@@ -7,12 +7,17 @@ angular.module('mean.gitstats').controller('GitstatsController', ['$scope', 'Glo
       name: 'gitstats'
     };
     $scope.message = 'No Message Yet...';
+    $scope.git_dev_username = null;
     
     
     $scope.getMessage = function() {
       Gitstats.get(function(data) {
         $scope.message = data.msg;
       });
+    };
+    
+    $scope.getDev = function() {
+      console.log($scope.git_dev_username);
     };
     
   }
--- git-dev-score/packages/custom/gitstats/server/tests/gitstats.js
+++ git-dev-score/packages/custom/gitstats/server/tests/gitstats.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Module dependencies
-var should = require('should'),
+/*var should = require('should'),
     mongoose = require('mongoose'),
     User = mongoose.model('User');
-
+*/
