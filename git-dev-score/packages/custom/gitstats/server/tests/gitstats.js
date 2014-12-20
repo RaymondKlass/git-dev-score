5fc9aab34a48236ca445b6b16e4e43b637960e19
RaymondKlass <raymond@klassphoto.com>
2014-12-20 15:34:41 -0500

    create test for saving git developer - server

--- git-dev-score/packages/custom/gitstats/server/tests/gitstats.js
+++ git-dev-score/packages/custom/gitstats/server/tests/gitstats.js
@@ -1,7 +1,37 @@
 'use strict';
 
 // Module dependencies
-/*var should = require('should'),
+var should = require('should'),
     mongoose = require('mongoose'),
-    User = mongoose.model('User');
-*/
+    GitDev = mongoose.model('GitDev');
+
+// Globals
+var gitdev;
+
+
+// Test Suites
+describe('<Unit Test>', function() {
+    describe('Model GitDev', function() {
+        beforeEach(function(done) {
+            
+            gitdev = new GitDev({
+                username: 'Test Developer'
+            });
+            
+            done();
+        });
+        
+        describe('Method Save', function() {
+            it('Should be able to save new Git Developer', function(done) {
+                return gitdev.save(function(err) {
+                    should.not.exist(err);
+                    gitdev.username.should.equal('Test Developer');
+                    gitdev.created.should.not.have.length(0);
+                    done();
+                });
+            });
+        
+        });
+        
+    });
+});
