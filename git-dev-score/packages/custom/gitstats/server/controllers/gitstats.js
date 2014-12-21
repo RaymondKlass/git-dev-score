0bc72acd82f6d06772a3ebcdbe2a88e09d874e02
RaymondKlass <raymond@klassphoto.com>
2014-12-21 10:11:23 -0500

    update to use node-github instead of raw requests

--- git-dev-score/packages/custom/gitstats/server/controllers/gitstats.js
+++ git-dev-score/packages/custom/gitstats/server/controllers/gitstats.js
@@ -2,9 +2,10 @@
 
 var mongoose = require('mongoose'),
   GitDev = mongoose.model('GitDev'),
-  request = require('request'),
+  /*request = require('request'),*/
   /*async = require('async'),*/
-  gitapi = require('../config/git_api');
+  GitApiConfig = require('../config/git_api'),
+  GitHubApi = require('github');
 
 
 exports.ajax_test = function(req, res) {
@@ -23,13 +24,13 @@ exports.git_developer_lookup = function(req, res) {
     
   console.log(developer);
   
-  console.log(gitapi.git_base_url);
-  
+  //console.log(gitapi.git_base_url);
+  /*
   var query_string = {
     client_id: gitapi.client_id,
     client_secret: gitapi.client_secret
-  };
-  
+  }; */
+  /* -- uses request library - exchanging for github api - node library via npm
   request({url:gitapi.git_base_url + '/users/' + developer, qs:query_string, headers:{'User-Agent':gitapi.user_agent}}, function(err, response, body) {
     if (err) {
       console.log(err);
@@ -40,8 +41,30 @@ exports.git_developer_lookup = function(req, res) {
 
     res.json({status: 'Good'});
   
+  }); */
+  
+  var github = new GitHubApi({
+    version: '3.0.0',
+    headers: {
+      'user-agent': GitApiConfig.user_agent
+    }
   });
   
+  github.authenticate({
+    type: 'oauth',
+    key: GitApiConfig.client_id,
+    secret: GitApiConfig.client_secret
+  });
+  
+  github.user.getFrom({user:developer}, 
+    function(err, api_res) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(api_res);
+      }
+    }
+  );
   /*
   // Should probably upsert here instead...
   gitdev.save(function(err) {
